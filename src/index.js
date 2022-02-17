ea519ccebb35b5947c367df057732d68fd384665
Raghunandan <nandan.raghu002@gmail.com>
2022-02-17 17:05:38 -0500

    created seperate folder for homepage components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Homepage from './components/homepage';
+import Homepage from './components/homepage/homepage';
 
 ReactDOM.render(
   <Homepage/>,
